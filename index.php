f6d6498ce483e3c8a58c39be58b0c69af154403e
zmadhar <zamadhar@gmail.com>
2022-05-12 14:58:22 -0400

    added files into my repo

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+//Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+session_start();
+
+//autoload file
+require_once('vendor/autoload.php');
+
+
+//Create an instance of the Base class
+
+$f3 = Base::instance();
+
+
+//Define a default route
+$f3->route('GET /', function(){
+
+    $view = new Template();
+    echo $view->render('views/PetHome.html');
+});
+
+$f3->route('GET/order', function(){
+
+    $view = new Template();
+    echo $view->render('views/PetOrder.html');
+});
+
+$f3->route('POST /order2', function(){
+
+    // Store data in session
+    $_SESSION['animal'] = $_POST['animal'];
+    $_SESSION['color'] = $_POST['color'];
+
+   $view = new Template();
+    echo $view->render('views/PetOrder2.html');
+});
+
+$f3->route('POST /summary', function() {
+
+
+    $view = new Template();
+    echo $view->render('views/OrderSummary.html');
+});
+
+//Run fat free
+$f3->run();
